98f1217c9b630bd9022e06ded5a70ee2accc7185
Anamul029 <anamhaque.5101@gmail.com>
2025-03-14 15:45:13 +0600

    adding testimonial section

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,6 +3,7 @@ import './App.css'
 import Banner from './components/Home/Banner/Banner'
 import Footer from './components/Home/Footer/Footer'
 import NavBar from './components/Home/NavBar/NavBar'
+import Testimonials from './components/Home/Testimonials/Testimonials'
 
 function App() {
 
@@ -11,6 +12,7 @@ function App() {
       <div className='max-w-[1440px] mx-auto'>
         <NavBar />
         <Banner />
+        <Testimonials/>
         <Footer />
       </div>
     </>
