e3ddf555f004217853b9fc98ee9eff21ff29170b
Anamul029 <anamhaque.5101@gmail.com>
2025-05-13 11:31:16 +0600

    add register functionality

--- frontend/src/components/Authentication/Register/Register.jsx
+++ frontend/src/components/Authentication/Register/Register.jsx
@@ -1,50 +1,70 @@
+import { createUserWithEmailAndPassword } from "firebase/auth";
 import { NavLink } from "react-router-dom";
+import Auth from "../../../assets/Firebase/firebase.config";
 
 const Register = () => {
-    const handleRegister=e=>{
+
+    const handleRegister = e => {
         e.preventDefault();
+        const name=e.target.name.value;
+        const email=e.target.email.value;
+        const password=e.target.password.value;
+        const photo=e.target.photo.value;
+        createUserWithEmailAndPassword(Auth, email, password)
+            .then((userCredential) => {
+                // Signed up 
+                const user = userCredential.user;
+                console.log(user);
+                // ...
+            })
+            .catch((error) => {
+                const errorCode = error.code;
+                const errorMessage = error.message;
+                // ..
+                console.log(errorMessage,errorCode);
+            });
     }
     return (
         <div>
             <div className="hero min-h-screen bg-base-200">
-            <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
-                <form onSubmit={handleRegister} className="card-body">
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Name</span>
-                        </label>
-                        <input type="text" name="name" placeholder="name" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Email</span>
-                        </label>
-                        <input type="email" name="email" placeholder="email" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">PhotoURL</span>
-                        </label>
-                        <input type="text" name="photo" placeholder="photoURL" className="input input-bordered" required />
-                    </div>
-                    <div className="form-control">
-                        <label className="label">
-                            <span className="label-text">Password</span>
-                        </label>
-                        <input type="password" name="password" placeholder="password" className="input input-bordered" required />
-                        <label className="label">
-                            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-                        </label>
-                    </div>
-                    <div className="form-control mt-6">
-                        <button className="btn btn-primary">Register</button>
-                    </div>
-                    <p className="text-center">Already have an account?please <NavLink to="/login"><span className="font-bold text-green-700 border-b-4">Login</span></NavLink></p>
-                </form>
+                <div className="card shrink-0 w-full max-w-sm shadow-2xl bg-base-100">
+                    <form onSubmit={handleRegister} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Name</span>
+                            </label>
+                            <input type="text" name="name" placeholder="name" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="email" name="email" placeholder="email" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">PhotoURL</span>
+                            </label>
+                            <input type="text" name="photo" placeholder="photoURL" className="input input-bordered" required />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name="password" placeholder="password" className="input input-bordered" required />
+                            <label className="label">
+                                <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <button className="btn btn-primary">Register</button>
+                        </div>
+                        <p className="text-center">Already have an account?please <NavLink to="/login"><span className="font-bold text-green-700 border-b-4">Login</span></NavLink></p>
+                    </form>
+
+                </div>
 
             </div>
-
-        </div> 
         </div>
     );
 };
