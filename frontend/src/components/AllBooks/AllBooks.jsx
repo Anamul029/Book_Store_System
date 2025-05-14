a1d5cb4644339ca960ab5c3e510101dec98d09cc
Anamul029 <anamhaque.5101@gmail.com>
2025-05-14 13:57:41 +0600

    adding allbooks data api and connect to the frontend part

--- frontend/src/components/AllBooks/AllBooks.jsx
+++ frontend/src/components/AllBooks/AllBooks.jsx
@@ -1,8 +1,23 @@
+import { useState } from "react";
+import BookCard from "./BookCard";
+
 
 const AllBooks = () => {
+    const [books,setBooks]=useState([])
+    useState(()=>{
+     fetch('https://library-management-server-orcin.vercel.app/books')
+     .then(res=>res.json())
+     .then(data=>setBooks(data))
+    },[]);
+    console.log(books)
     return (
         <div>
-            <h1 className="pt-52">Hello From All Books Route </h1>
+            <h3 className="text-2xl font-semibold text-center mb-8">See all the books here:{books.length}</h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-8">
+                {
+                    books.map(info=><BookCard info={info} key={info._id}></BookCard>)
+                }
+            </div>
         </div>
     );
 };
